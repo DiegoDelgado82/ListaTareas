7c542bb3cd3d7291299499d6815597c46633189e
DiegoDelgado82 <diegodelgado365@gmail.com>
2022-04-19 11:11:01 -0300

    tercer rama

--- ListaDeTareas/main.js
+++ ListaDeTareas/main.js
@@ -25,7 +25,7 @@ addBtn.addEventListener("click", (e) => {
 function addDeleteBtn() {
   const deleteBtn = document.createElement("button");
 
-  deleteBtn.textContent = "X";
+  deleteBtn.textContent = "X-X";
   deleteBtn.className = "btn-delete";
 
   deleteBtn.addEventListener("click", (e) => {
